274892f16a58ec609d1dee082adf714794050ae0
Dennis Dyodi Kawakami <denniskawakami@gmail.com>
2020-06-08 20:40:03 -0300

    Finalizando o projeto

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const server = express()
 
+//Pegar o banco de dados
+const db = require('./database/db')
+
+//Habilitar  o uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
 //configurar pasta públic
 server.use(express.static("public"))
 
@@ -22,8 +28,63 @@ server.get('/create-point', (req, res) => {
   return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+  //req.body: corpo do formulário
+
+  //inserir dados no banco de dados
+  const query = `
+  INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+  ) VALUES (?,?,?,?,?,?,?);
+`
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err)
+      return res.send("Erro no cadastro")
+    }
+
+    console.log("Cadastro realizado com sucesso!")
+    console.log(this)
+    return res.render("create-point.html", { saved: true })
+  }
+
+  db.run(query, values, afterInsertData)
+
+})
+
 server.get('/search', (req, res) => {
-  return res.render("search-results.html")
+  const search = req.query.search
+
+  if (search == "") {
+    return res.render("search-results.html", { total: 0 })
+  }
+  //Pegar dados do banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+    if (err) {
+      return console.log(err)
+    }
+
+    const total = rows.length
+
+    //Mostrar página html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total })
+  })
 })
 
 server.listen(3000)
